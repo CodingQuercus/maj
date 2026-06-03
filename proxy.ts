2dbecd781a9aaa829333d970107c1a7eac38c58a
CodingQuercus <123453349+CodingQuercus@users.noreply.github.com>
2026-06-03 13:52:48 +0200

    fix: add /privacy to middleware public routes.

--- proxy.ts
+++ proxy.ts
@@ -47,6 +47,7 @@ export async function proxy(request: NextRequest) {
         !user &&
         !request.nextUrl.pathname.startsWith('/login') &&
         !request.nextUrl.pathname.startsWith('/auth') &&
+        !request.nextUrl.pathname.startsWith('/privacy') &&
         request.nextUrl.pathname !== '/'
     ) {
         const url = request.nextUrl.clone();
